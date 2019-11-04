5d767891c759016076b702ddfa1412004f6d655c
NAGATA Minoru <nagata_minoru@yahoo.co.jp>
2019-11-04 13:45:40 +0900

    クリックしたらマークを変える

--- tweet.jsx
+++ tweet.jsx
@@ -1,6 +1,8 @@
 function Tweet(props) {
   const [liked, setLike] = React.useState(false);
 
+  const toggleLike = React.useCallback(() => setLike(prev => !prev), [setLike]);
+
   return (
     <div className='tweet'>
       <div className='icon_container'>{props.icon}</div>
@@ -14,7 +16,7 @@ function Tweet(props) {
         <div className='content'>{props.content}</div>
 
         <div className='status_action'>
-          <span>{liked ? '\u{2764}' : '\u{2661}'}</span>
+          <span onClick={toggleLike}>{liked ? '\u{2764}' : '\u{2661}'}</span>
         </div>
       </div>
     </div>
