64e7e5714b79c8ca8e696e2ea0ae18cdf897ddd1
NAGATA Minoru <nagata_minoru@yahoo.co.jp>
2019-11-04 13:28:10 +0900

    いいね表示

--- tweet.jsx
+++ tweet.jsx
@@ -1,13 +1,21 @@
 function Tweet(props) {
+  const [liked, setLike] = React.useState(false);
+
   return (
     <div className='tweet'>
       <div className='icon_container'>{props.icon}</div>
+
       <div className='body_container'>
         <div className='status_display'>
           <span className='display_name'>{props.display_name}</span>
           <span className='account_name'>@{props.account_name}</span>
         </div>
+
         <div className='content'>{props.content}</div>
+
+        <div className='status_action'>
+          <span>{liked ? '\u{2764}' : '\u{2661}'}</span>
+        </div>
       </div>
     </div>
   );
